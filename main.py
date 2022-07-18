c05ab16224df858d6b8c36f099d149c18d2c59fb
Parusha Pradhan <parusha@Parushas-MacBook-Pro.local>
2022-07-18 15:08:42 +0545

    overall delay

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 
-import socket
+from socket import *
+from time import time, ctime
+import sys
 
 msgFromClient       = "Hello UDP Server"
 bytesToSend         = str.encode(msgFromClient)
 serverAddressPort   = ("ec2-3-6-93-42.ap-south-1.compute.amazonaws.com", 11023)
-bufferSize          = 1024
 
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-msg = "Message from Server {}".format(msgFromServer[0])
+UDPClientSocket = socket(family=AF_INET, type=SOCK_DGRAM)
 
-print(msg)
+UDPClientSocket.settimeout(1)
+for i in range(10):
+    startTime = time()
+    message = "Ping " + str(i+1) + " " + ctime(startTime)[11:19]
+
+    try:
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        encodedModified, serverAddress = UDPClientSocket.recvfrom(bufferSize)
+        endTime = time()
+        modifiedMessage = encodedModified.decode()
+        print(modifiedMessage)
+        print("Overall delay: %.3f ms\n" % ((endTime - startTime)*1000))
+    except:
+        print("PING %i Request timed out\n" % (i+1))
+
+UDPClientSocket.close()
